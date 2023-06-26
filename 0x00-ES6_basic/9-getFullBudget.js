660ba3557b4183362e0e4d0d8c3a346a1f60d680
Denniskwg <dkamau476@gmail.com>
2023-06-26 22:52:32 +0300

    adding solution for question 5-12

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,16 @@
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(value) {
+      return `$${value}`;
+    },
+    getIncomeInEuros(euros) {
+      return `${euros} euros`;
+    },
+  };
+
+  return fullBudget;
+}
