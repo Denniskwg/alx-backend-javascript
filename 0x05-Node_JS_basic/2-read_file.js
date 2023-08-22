795600d40c88b11ae6d9eae06e178259be4a5a26
Denniskwg <dkamau476@gmail.com>
2023-08-22 14:23:02 +0300

    Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,33 @@
+const { spawnSync } = require('node:child_process');
+
+function countStudents(path) {
+  try {
+    const cat = spawnSync('cat', [path]);
+    if (cat.status !== 0) {
+      throw new Error('Cannot load the database');
+    }
+    const arr = cat.stdout.toString().split('\n');
+    const newArr = arr.slice(1, arr.length - 1);
+    console.log(`Number of students: ${newArr.length}`);
+    const vector = newArr.map((item) => item.split(','));
+    const cs = [];
+    const swe = [];
+    let csNum = 0;
+    let sweNum = 0;
+    vector.forEach((item) => {
+      if (item.includes('CS')) {
+        cs.push(item[0]);
+        csNum += 1;
+      } else {
+        swe.push(item[0]);
+        sweNum += 1;
+      }
+    });
+    console.log(`Number of students in CS: ${csNum}. List: ${cs.join(', ')}`);
+    console.log(`Number of students in SWE: ${sweNum}. List: ${swe.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
