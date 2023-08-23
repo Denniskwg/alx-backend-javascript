5815286942f305c2f3aba1261993194e4d0864bd
Denniskwg <dkamau476@gmail.com>
2023-08-23 11:05:55 +0300

    corrections

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
-let path = process.argv[2];
+const path = process.argv[2];
 
 const hostname = '127.0.0.1';
 const port = 1245;
@@ -11,29 +11,29 @@ const app = http.createServer((req, res) => {
   if (url === '/') {
     res.end('Hello Holberton School!');
   } else if (url === '/students') {
+    let result = '';
     if (!fs.existsSync(path)) {
-      path = 'database.csv';
+      result = 'Cannot load the database';
+    } else {
+      const arr = fs.readFileSync(path, 'utf-8').split('\n');
+      const newArr = arr.slice(1, arr.length - 1);
+      const vector = newArr.map((item) => item.split(','));
+      const cs = [];
+      const swe = [];
+      let csNum = 0;
+      let sweNum = 0;
+      vector.forEach((item) => {
+        if (item.includes('CS')) {
+          cs.push(item[0]);
+          csNum += 1;
+        } else {
+          swe.push(item[0]);
+          sweNum += 1;
+        }
+      });
+      result = `This is the list of our students\nNumber of students: ${newArr.length}\nNumber of students in CS: ${csNum}. List: ${cs.join(', ')}\nNumber of students in SWE: ${sweNum}. List: ${swe.join(', ')}`;
     }
-    if (!fs.statSync(path).isFile()) {
-      path = 'database.csv';
-    }
-    const arr = fs.readFileSync(path, 'utf-8').split('\n');
-    const newArr = arr.slice(1, arr.length - 1);
-    const vector = newArr.map((item) => item.split(','));
-    const cs = [];
-    const swe = [];
-    let csNum = 0;
-    let sweNum = 0;
-    vector.forEach((item) => {
-      if (item.includes('CS')) {
-        cs.push(item[0]);
-        csNum += 1;
-      } else {
-        swe.push(item[0]);
-        sweNum += 1;
-      }
-    });
-    res.end(`This is the list of our students\nNumber of students: ${newArr.length}\nNumber of students in CS: ${csNum}. List: ${cs.join(', ')}\nNumber of students in SWE: ${sweNum}. List: ${swe.join(', ')}`);
+    res.end(result);
   }
 });
 
