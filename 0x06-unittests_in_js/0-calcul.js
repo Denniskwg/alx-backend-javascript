675c4933e415c9d80882b7e6a2369fa0376804a8
Denniskwg <dkamau476@gmail.com>
2023-09-22 16:39:55 +0300

    corrections 2

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,10 +1,6 @@
-function roundNumber(number) {
-  return Math.round(number);
-}
-
 function calculateNumber(a, b) {
-  const x = roundNumber(a);
-  const y = roundNumber(b);
+  const x = Math.round(a);
+  const y = Math.round(b);
   return x + y;
 }
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,24 +4,23 @@ const calculateNumber = require('./0-calcul');
 describe('test calculateNumber', () => {
 
   it('first number rounded', () => {
-    let num1 = 1.6;
+    let num1 = 1.2;
     let num2 = 2.6;
     const result = calculateNumber(num1, num2);
-    assert.strictEqual(result - 3, 2);
+    assert.strictEqual(result, 4);
   });
 
   it('second number rounded', () => {
-    let num1 = 1.6;
-    let num2 = 2.6;
+    let num1 = 1.2;
+    let num2 = 2.2;
     const result = calculateNumber(num1, num2);
-    assert.strictEqual(result - 2, 3);
+    assert.strictEqual(result, 3);
   });
 
   it('both numbers rounded', () => {
-    let num1 = 1.6;
-    let num2 = 2.6;
+    let num1 = 1.2;
+    let num2 = 2.2;
     const result = calculateNumber(num1, num2);
-    assert.strictEqual(result - 2, 3);
-    assert.strictEqual(result - 3, 2);
+    assert.strictEqual(result, 3);
   });
 });
