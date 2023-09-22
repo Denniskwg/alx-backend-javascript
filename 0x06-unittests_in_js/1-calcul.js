ee7dc775dc0d857b6fa674d7f911c44c95835e16
Denniskwg <dkamau476@gmail.com>
2023-09-22 18:23:17 +0300

    test suite 2

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,21 @@
+function calculateNumber(a, b, type) {
+  if (type === 'SUM') {
+    const x = Math.round(a);
+    const y = Math.round(b);
+    return x + y;
+  }
+  if (type === 'SUBTRACT') {
+    const x = Math.round(a);
+    const y = Math.round(b);
+    return y - x;
+  }
+  if (type === 'DIVIDE') {
+    const x = Math.round(a);
+    const y = Math.round(b);
+    if (y === 0) {
+      return 'Error';
+    }
+    return x / y;
+  }
+}
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('test calculateNumber', () => {
+
+  it('test suite for type=SUM', () => {
+    let num1 = 1.2;
+    let num2 = 2.2;
+    const result = calculateNumber(num1, num2, 'SUM');
+    assert.strictEqual(result, 3);
+  });
+
+  it('test suite for type=SUBTRACT', () => {
+    let num1 = 1.2;
+    let num2 = 2.2;
+    const result = calculateNumber(num1, num2, 'SUBTRACT');
+    assert.strictEqual(result, 1);
+  });
+
+  it('test suite for type=DIVIDE', () => {
+    let num1 = 1.2;
+    let num2 = 2.2;
+    const result = calculateNumber(num1, num2, 'DIVIDE');
+    assert.strictEqual(result, 0.5);
+  });
+
+  it('test suite for type=DIVIDE', () => {
+    let num1 = 1.2;
+    let num2 = 0.2;
+    const result = calculateNumber(num1, num2, 'DIVIDE');
+    assert.strictEqual(result, 'Error');
+  });
+});
