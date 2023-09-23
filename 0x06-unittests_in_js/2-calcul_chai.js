cf0a70e05093977b420cdbcc723abdd6ff94d08c
Denniskwg <dkamau476@gmail.com>
2023-09-23 12:42:26 +0300

    third test suite

---
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,21 @@
+function calculateNumber(type, a, b) {
+  if (type === 'SUM') {
+    const x = Math.round(a);
+    const y = Math.round(b);
+    return x + y;
+  }
+  if (type === 'SUBTRACT') {
+    const x = Math.round(a);
+    const y = Math.round(b);
+    return x - y;
+  }
+  if (type === 'DIVIDE') {
+    const x = Math.round(a);
+    const y = Math.round(b);
+    if (y === 0) {
+      return 'Error';
+    }
+    return x / y;
+  }
+}
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+const chai = require('chai');
+
+describe('test calculateNumber', () => {
+
+  it('test suite for type=SUM', () => {
+    let num1 = 1.2;
+    let num2 = 2.2;
+    const result = calculateNumber('SUM', num1, num2);
+    chai.expect(result).to.equal(3);
+  });
+
+  it('test suite for type=SUBTRACT', () => {
+    let num1 = 1.2;
+    let num2 = 2.2;
+    const result = calculateNumber('SUBTRACT', num1, num2);
+    chai.expect(result).to.equal(-1);
+  });
+
+  it('test suite for type=DIVIDE', () => {
+    let num1 = 1.2;
+    let num2 = 2.2;
+    const result = calculateNumber('DIVIDE', num1, num2);
+    chai.expect(result).to.equal(0.5);
+  });
+
+  it('test suite for type=DIVIDE', () => {
+    let num1 = 1.2;
+    let num2 = 0.2;
+    const result = calculateNumber('DIVIDE', num1, num2);
+    chai.expect(result).to.equal('Error');
+  });
+});
