44b24c2c60733e64b11718a19a90a5d72ee9a4cc
Denniskwg <dkamau476@gmail.com>
2023-09-25 11:10:05 +0300

    corrections

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -5,30 +5,35 @@ const chai = require('chai');
 describe('test calculateNumber', () => {
 
   it('test suite for type=SUM', () => {
-    let num1 = 1.2;
-    let num2 = 2.2;
-    const result = calculateNumber('SUM', num1, num2);
-    chai.expect(result).to.not.equal(num1 * num2)
-    chai.expect(result).to.not.equal(num1 - num2)
-    chai.expect(result).to.not.equal(num1 / num2)
+    chai.expect(calculateNumber('SUM', 2.0, 2.0)).to.equal(4)
+    chai.expect(calculateNumber('SUM', 2.3, 1.8)).to.equal(4)
+    chai.expect(calculateNumber('SUM', -2.0, -2.0)).to.equal(-4)
+    chai.expect(calculateNumber('SUM', -2.3, -1.8)).to.equal(-4);
+    chai.expect(calculateNumber('SUM', 2.0, -2.0)).to.equal(0);
+    chai.expect(calculateNumber('SUM', 0.0, 0.0)).to.equal(0);
   });
 
   it('test suite for type=SUBTRACT', () => {
-    let num1 = 1.2;
-    let num2 = 2.2;
-    const result = calculateNumber('SUBTRACT', num1, num2);
-    chai.expect(result).to.not.equal(num1 + num2)
-    chai.expect(result).to.not.equal(num1 * num2)
-    chai.expect(result).to.not.equal(num1 / num2)
+    chai.expect(calculateNumber('SUBTRACT', 2.0, 2.0)).to.equal(0);
+    chai.expect(calculateNumber('SUBTRACT', 2.3, 1.8)).to.equal(0);
+    chai.expect(calculateNumber('SUBTRACT', -2.0, -2.0)).to.equal(0);
+    chai.expect(calculateNumber('SUBTRACT', -2.3, -1.8)).to.equal(0);
+    chai.expect(calculateNumber('SUBTRACT', -2.0, 2.0)).to.equal(-4.0);
+    chai.expect(calculateNumber('SUBTRACT', 2.0, -2.0)).to.equal(4.0);
+    chai.expect(calculateNumber('SUBTRACT', 0.0, 0.0)).to.equal(0);
   });
 
   it('test suite for type=DIVIDE', () => {
-    let num1 = 1.2;
-    let num2 = 2.2;
-    const result = calculateNumber('DIVIDE', num1, num2);
-    chai.expect(result).to.not.equal(num1 + num2)
-    chai.expect(result).to.not.equal(num1 * num2)
-    chai.expect(result).to.not.equal(num1 - num2)
+    chai.expect(calculateNumber('DIVIDE', 8.0, 2.0)).to.equal(4.0);
+    chai.expect(calculateNumber('DIVIDE', -7.0, 2.0)).to.equal(-3.5);
+    chai.expect(calculateNumber('DIVIDE', 7.0, -2.0)).to.equal(-3.5);
+    chai.expect(calculateNumber('DIVIDE', -7.0, -2.0)).to.equal(3.5);
+    chai.expect(calculateNumber('DIVIDE', 2.0, 2.0)).to.equal(1);
+    chai.expect(calculateNumber('DIVIDE', -2.0, -2.0)).to.equal(1);
+    chai.expect(calculateNumber('DIVIDE', 2.6, 3.0)).to.equal(1);
+    chai.expect(calculateNumber('DIVIDE', 2.4, 2.0)).to.equal(1);
+    chai.expect(calculateNumber('DIVIDE', 0.0, 5.0)).to.equal(0);
+    chai.expect(calculateNumber('DIVIDE', 0.0, -5.0)).to.equal(-0);
   });
 
   it('test suite for type=DIVIDE', () => {
